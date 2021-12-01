b49d8357ee2e65ce4987970390959e3f65969722
charl <charlos51ht@gmail.com>
2021-12-01 12:00:37 -0500

    Commit

--- BandView/urls.py
+++ BandView/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('welcome/', views.welcome, name="welcome"),
+    path('', views.welcome, name="welcome"),
     path('sign-in/', views.signin, name="sign-in"),
     path('band-sign-up/', views.bandsignup, name="band-sign-up")
 ]
